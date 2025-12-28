a4330d0221c35b9a75f04e93d4208391341a818a
Aky33 <tom.zaliger@gmail.com>
2025-12-28 20:46:42 +0100

    Úprava configů abychom rozeběhli testy a nerozbili aplikaci

--- src-be/jest.config.ts
+++ src-be/jest.config.ts
@@ -1,8 +1,20 @@
-export default {
-    preset: 'ts-jest',
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
     clearMocks: true,
     testMatch: [
         '**/*-test.ts',
     ],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1'
+    },
+    globals: {
+        'ts-jest': {
+            useESM: true
+        }
+    }
 };
+
+export default config;
